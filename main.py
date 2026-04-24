62efc11d6b4272889c6a9f7f8992da6071baf80c
lUCA <LUCABAKSHI98@GMAIL.com>
2026-04-24 12:45:52 +0800

    Updating initial authentication script

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 
+headers = {
+    "APCA-API-KEY-ID": "PKPY4KHQBW3BEJ765NIIF7NKKK",
+    "APCA-API-SECRET-KEY": "9B1exyYHkXehHWmFkDs7LcYHpL2eqQr9hLi5sBRVVdT"
+}
 
-r = requests.get('https://paper-api.alpaca.markets/v2/apiKey=PK6ZO5SRD7UEALG2KP7ZSVJOC6')
+r = requests.get('https://paper-api.alpaca.markets/v2/account', headers=headers)
 content = r.json()
 
+print(r.status_code)
+print(r.text)
 
 print(content)
